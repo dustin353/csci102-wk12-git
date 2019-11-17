5e95f38418357e0b0f63016f0c525aa04527f3b4
Dustin Choat <dustinchoat@mymail.mines.edu>
2019-11-17 11:56:40 -0700

    added function 2

--- Week12-utility.py..py
+++ Week12-utility.py..py
@@ -3,6 +3,24 @@
 # CSCI 102 - Section B
 # Week 11 - Part B
 
-#1. Print Output
+#1. PrintOutput
 def PrintOutput(put):
     print('OUTPUT', put)
+
+#2. LoadFile
+def LoadFile(filename):
+    file = open(filename)
+    contents = file.read()
+    list1 = contents.split('\n')
+    ''' Attempt to put list elements between double quotation marks
+    x = 0
+    string1 = ''
+    for x in range(len(list1)):
+        string1 = string1 + '"' + list1[x] + '"' + '-'
+        x += 1
+    print(string1)
+    list2 = string1.split('-')
+    print(list2)
+    '''
+    return list1
+    
